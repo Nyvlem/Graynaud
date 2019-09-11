dbff295e5d4df4d45f01e288518b8a19f06a35ae
Nyvlem <Nyvlem59@hotmail.fr>
2019-09-12 00:15:59 +0200

    Config
    add config support

--- src/main/java/fr/nyvlem/graynaud/Graynaud.java
+++ src/main/java/fr/nyvlem/graynaud/Graynaud.java
@@ -1,6 +1,10 @@
 package fr.nyvlem.graynaud;
 
+import fr.nyvlem.graynaud.gamemode_inventory.GameModeInventoryListener;
 import fr.nyvlem.graynaud.lavaswim.Lavaswimlistener;
+import fr.nyvlem.graynaud.utils.Constants;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -14,13 +18,15 @@ public class Graynaud extends JavaPlugin {
 		loadConfig();
 
 		boolean enableGamemodeInventory = config.getBoolean(Constants.GAMEMODE_INVETORY_CONFIG_PATH, false);
+		boolean enableLavaswim = config.getBoolean(Constants.LAVASWIM_CONFIG_PATH, false);
 
 		if (enableGamemodeInventory) {
 			getServer().getPluginManager().registerEvents(new GameModeInventoryListener(this), this);
 		}
-
+		if (enableLavaswim) {
+			getServer().getPluginManager().registerEvents(new Lavaswimlistener(), this);
+		}
 		getLogger().info("Graynaud enabled !");
-		getServer().getPluginManager().registerEvents(new Lavaswimlistener(), this);
 	}
 
 	private void loadConfig() {
