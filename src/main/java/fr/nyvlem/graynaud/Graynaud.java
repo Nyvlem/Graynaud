3b47fa1341fe206e3770bb345a8b7617ee3dafc3
Gaétan RAYNAUD <gaetanraynaud@outlook.com>
2019-09-11 23:41:44 +0200

    Add gamemode inventory feature

--- src/main/java/fr/nyvlem/graynaud/Graynaud.java
+++ src/main/java/fr/nyvlem/graynaud/Graynaud.java
@@ -1,16 +1,37 @@
 package fr.nyvlem.graynaud;
 
+import fr.nyvlem.graynaud.gamemode_inventory.GameModeInventoryListener;
+import fr.nyvlem.graynaud.utils.Constants;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class Graynaud extends JavaPlugin {
 
+	private FileConfiguration config;
+
 	@Override
 	public void onEnable() {
-		getLogger().info("Plugin lancé chef ! graynaud paré a l'attaque !");
+		loadConfig();
+
+		boolean enableGamemodeInventory = config.getBoolean(Constants.GAMEMODE_INVETORY_CONFIG_PATH, false);
+
+		if (enableGamemodeInventory) {
+			getServer().getPluginManager().registerEvents(new GameModeInventoryListener(this), this);
+		}
+
+		getLogger().info("Graynaud enabled !");
 	}
-	
-	@Override
-	public void onDisable() {
-		getLogger().info("Ok on remballe.");
+
+	private void loadConfig() {
+		File file = new File(getDataFolder(), Constants.CONFIG_FILE);
+
+		if (!file.exists()) {
+			saveResource(Constants.CONFIG_FILE, false);
+		}
+
+		config = YamlConfiguration.loadConfiguration(file);
 	}
 }
---
+++ src/main/java/fr/nyvlem/graynaud/utils/Utils.java
@@ -0,0 +1,20 @@
+package fr.nyvlem.graynaud.utils;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class Utils {
+
+    private Utils() {}
+
+    public static File getDataFileForPlayer(JavaPlugin plugin, Player player) {
+        return new File(plugin.getDataFolder() + File.separator + Constants.DATA_FOLDER + File.separator + player.getUniqueId() + ".yml");
+    }
+
+    public static String getInventoryContentPath(GameMode gameMode) {
+        return "inventory." + gameMode.name() + ".content";
+    }
+}
