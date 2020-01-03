2b1fc3830e34e7eb4fc42e35377e0abb735cdf01
Nyvlem <Nyvlem59@hotmail.fr>
2020-01-04 00:01:42 +0100

    Add base subclass for our partys

---
+++ src/main/java/fr/nyvlem/graynaud/mcMMO/GraynaudParty.java
@@ -0,0 +1,11 @@
+package fr.nyvlem.graynaud.mcMMO;
+
+import com.gmail.nossr50.datatypes.party.Party;
+
+public class GraynaudParty extends Party {
+
+    public GraynaudParty(String name) {
+        super(name);
+    }
+
+}
