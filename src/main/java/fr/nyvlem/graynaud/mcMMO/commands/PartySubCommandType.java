02efc86d96be00d020f4f1df947b24229f686416
Nyvlem <Nyvlem59@hotmail.fr>
2020-01-05 18:07:01 +0100

    GetSubCommand deleted

--- src/main/java/fr/nyvlem/graynaud/mcMMO/commands/PartySubCommandType.java
+++ src/main/java/fr/nyvlem/graynaud/mcMMO/commands/PartySubCommandType.java
@@ -2,13 +2,4 @@ package fr.nyvlem.graynaud.mcMMO.commands;
 
 public enum PartySubCommandType {
     SPAWN, SETSPAWN;
-
-    public static PartySubCommandType getSubCommand(String commandName) {
-        for (PartySubCommandType command : values()) {
-            if (command.name().equalsIgnoreCase(commandName)) {
-                return command;
-            }
-        }
-        return null;
-    }
 }
