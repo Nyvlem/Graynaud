a88b5b26b81a93acf76f377220e92ee72c442789
Nyvlem <Nyvlem59@hotmail.fr>
2020-01-05 17:46:50 +0100

    Missed a review

--- src/main/java/fr/nyvlem/graynaud/mcMMO/commands/PartyCommand.java
+++ src/main/java/fr/nyvlem/graynaud/mcMMO/commands/PartyCommand.java
@@ -6,13 +6,23 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PartyCommand implements TabExecutor {
 
+    private static final List<String> PARTY_SUB_COMMANDS;
+
     private CommandExecutor partySetSpawnCommand = new PartySetSpawnCommand();
     private CommandExecutor partySpawnCommand = new PartySpawnCommand();
+
+    static {
+        PARTY_SUB_COMMANDS = Arrays.stream(PartySubCommandType.values()).map(Enum::toString).sorted().collect(Collectors.toList());
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -31,11 +41,17 @@ public class PartyCommand implements TabExecutor {
             default:
                 break;
         }
+
+
         return false;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        switch (args.length) {
+            case 1:
+                return StringUtil.copyPartialMatches(args[0], PARTY_SUB_COMMANDS, new ArrayList<>(PARTY_SUB_COMMANDS.size()));
+        }
         return null;
     }
 }
